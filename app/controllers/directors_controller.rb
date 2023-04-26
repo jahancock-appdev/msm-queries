0cb3533155f5db9b455e364e38fe6bf484f389eb
Jordan Hancock <jhancock@chicagobooth.edu>
2023-04-26 00:12:22 +0000

    Eldest completed

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,5 +6,10 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/index.html.erb"})
   end
 
+  def eldest
 
+    @oldest_director = Director.where.not({ :dob => nil }).order( { :dob => :asc }).first
+    
+    render({ :template => "director_templates/eldest.html.erb"})
+  end
 end
