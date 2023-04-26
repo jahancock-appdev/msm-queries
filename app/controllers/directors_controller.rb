3296c1465de896b70ddac678628e5dee325f546e
Jordan Hancock <jhancock@chicagobooth.edu>
2023-04-26 02:02:18 +0000

    Youngest complete

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -13,11 +13,20 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/eldest.html.erb"})
   end
 
+
+  def youngest
+
+    @youngest_director = Director.where.not({ :dob => nil }).order( { :dob => :desc }).first
+    
+    render({ :template => "director_templates/youngest.html.erb"})
+  end
+
   def director_details
 
     the_id = params.fetch("an_id").to_i
 
-    @the_director = Director.where({:id => 1}).first
+    @the_director = Director.where({:id => the_id}).first
+    @filmography = Movie.where({:director_id => the_id})
 
     render({ :template => "director_templates/show.html.erb"})
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/eldest", { :controller => "directors", :action => "eldest" })
+  get("/directors/youngest", { :controller => "directors", :action => "youngest" })
+
   
   #Note that rails finds matches in routes from top to bottom which is why won't match eldest
   #Static routes must come above dynamic routes
